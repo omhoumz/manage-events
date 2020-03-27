89b93615f86bb08a4896bb771e7f97039bb6eaed
Omar Houmz <omhoumz@gmail.com>
2020-03-27 20:44:48 +0100

    chore: init fb db script

---
+++ core/db/db.js
@@ -0,0 +1,29 @@
+import firebase from '@firebase/app'
+import '@firebase/firestore'
+
+export function loadDB() {
+  try {
+    var config = {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+    }
+
+    firebase.initializeApp(config)
+  } catch (err) {
+    const isHotReloadingError =
+      /already exists/.test(err.message) &&
+      process.env.NODE_ENV === 'development'
+
+    if (!isHotReloadingError) {
+      console.error('Firebase initialization error', err.stack)
+    }
+  }
+
+  return firebase
+}
