62689b1932105a202ccb0450a82883bd21accd2a
Omar Houmz <omhoumz@gmail.com>
2020-03-31 22:24:20 +0100

    chore: new button props

--- components/ui/button/button.jsx
+++ components/ui/button/button.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 import { ButtonWrapper } from './button.styled'
 
-const Button = ({ children }) => {
-  return <ButtonWrapper>{children}</ButtonWrapper>
+const Button = ({ children, type = 'button', onClick }) => {
+  const btnProps = { type, onClick }
+  return <ButtonWrapper {...btnProps}>{children}</ButtonWrapper>
 }
 
 export default Button
